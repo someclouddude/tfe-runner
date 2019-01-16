99cc34cdfa77feedfbffd2f0a6483661e9dec8f3
someclouddude <42011691+someclouddude@users.noreply.github.com>
2019-01-15 21:47:52 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,5 +4,7 @@ module "ec2" {
   source  = "app.terraform.io/scdt/ec2/aws"
   version = "1.0.2"
 
-  ssh_key = "GeneralPlayground"
+  aws_key = "AKIAJPARU4UIFB3QLIDA"
+  aws_secret = "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"
+  ssh_key = "General-playground"
 }
